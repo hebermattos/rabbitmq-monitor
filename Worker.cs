7b7897a9d235ba38e0de42a9a5b9acf63471defb
heber <hebermattos@outlook.com>
2022-05-31 20:00:52 -0300

    docker

--- Worker.cs
+++ Worker.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics;
 using Newtonsoft.Json;
 
 namespace rabbitmq.monitor;
@@ -16,18 +17,23 @@ public class Worker : BackgroundService
         var rulesConfiguration = JsonConvert.DeserializeObject<RulesConfiguration>(await File.ReadAllTextAsync("configuration/rules.json"));
 
         var consoleLog = new ConsoleLogger();
-        var webhook = new WebhookSender(rulesConfiguration,_httpClientFactory);
+        var webhook = new WebhookSender(rulesConfiguration, _httpClientFactory);
 
         var queueRuleManager = new RuleManager<QueueDto>(rulesConfiguration, "queues", _httpClientFactory);
         queueRuleManager.AddRule(new ConsumerQuantity(rulesConfiguration));
         queueRuleManager.AddRule(new QueueType(rulesConfiguration));
-        queueRuleManager.AddAlert(consoleLog);
         queueRuleManager.AddAlert(webhook);
 
         var nodeRuleManager = new RuleManager<NodeDto>(rulesConfiguration, "nodes", _httpClientFactory);
         nodeRuleManager.AddRule(new NodeRunning(rulesConfiguration));
-        nodeRuleManager.AddAlert(consoleLog);
+
         nodeRuleManager.AddAlert(webhook);
+
+        if (Debugger.IsAttached)
+        {
+            queueRuleManager.AddAlert(consoleLog);
+            nodeRuleManager.AddAlert(consoleLog);
+        }
 
         var ruleManagers = new List<IRuleManager>();
         ruleManagers.Add(queueRuleManager);
