055f24c870f1a305c378d7e66743655fe855e922
Heber Mattos <heber.mattos@ilegra.com>
2022-06-03 18:50:10 -0300

    rabbitmq provider

--- Worker.cs
+++ Worker.cs
@@ -7,11 +7,13 @@ public class Worker : BackgroundService
 {
     private readonly IHttpClientFactory _httpClientFactory;
     private readonly IConfigurationRepository _configurationRepository;
+    private readonly IRabbitmqProvider _rabbitmqProvider;
 
-    public Worker(IHttpClientFactory httpClientFactory, IConfigurationRepository configurationRepository)
+    public Worker(IHttpClientFactory httpClientFactory, IConfigurationRepository configurationRepository, IRabbitmqProvider rabbitmqProvider)
     {
         _httpClientFactory = httpClientFactory;
         _configurationRepository = configurationRepository;
+        _rabbitmqProvider = rabbitmqProvider;
     }
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
@@ -21,12 +23,12 @@ public class Worker : BackgroundService
         var consoleLog = new ConsoleLogger();
         var webhook = new WebhookSender(rulesConfiguration, _httpClientFactory);
 
-        var queueRuleManager = new RuleManager<QueueDto>(rulesConfiguration, "queues", _httpClientFactory);
+        var queueRuleManager = new RuleManager<QueueDto>(rulesConfiguration, "queues", _rabbitmqProvider);
         queueRuleManager.AddRule(new ConsumerQuantity());
         queueRuleManager.AddRule(new QueueType());
         queueRuleManager.AddAlert(webhook);
 
-        var nodeRuleManager = new RuleManager<NodeDto>(rulesConfiguration, "nodes", _httpClientFactory);
+        var nodeRuleManager = new RuleManager<NodeDto>(rulesConfiguration, "nodes", _rabbitmqProvider);
         nodeRuleManager.AddRule(new NodeRunning());
 
         nodeRuleManager.AddAlert(webhook);
