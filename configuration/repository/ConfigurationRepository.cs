c6cd83e9c0f0b5628a5283d767852a50f7032c7f
Heber Mattos <heber.mattos@ilegra.com>
2022-06-03 18:38:31 -0300

    configuration

---
+++ configuration/repository/ConfigurationRepository.cs
@@ -0,0 +1,12 @@
+using Newtonsoft.Json;
+
+namespace rabbitmq.monitor
+{
+    public class ConfigurationRepository: IConfigurationRepository
+    {
+        public async Task<RulesConfiguration> Get()
+        {
+            return JsonConvert.DeserializeObject<RulesConfiguration>(await File.ReadAllTextAsync("configuration/rules.json"));   
+        }
+    }
+}
